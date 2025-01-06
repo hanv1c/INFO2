d111fc41f6f6500f91a2389ddf11b5d06d768fd7
hanv1c <160732313+hanv1c@users.noreply.github.com>
2025-01-06 11:55:17 -0300

    Add files via upload

---
+++ projetophyton/teste01.py
@@ -0,0 +1,12 @@
+nome = input("Qual é o seu nome? ")
+idade = input("Quantos anos você tem? ")
+print(f"Olá, {nome}! Você tem {idade} anos.")
+
+print("_______________________")
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+print(f"Soma: {num1 + num2}")
+print(f"Diferença: {num1 - num2}")
+print(f"Produto: {num1 * num2}")
+
