accbe67b2ab567b4b81ad7e2cc3db78d3764f49a
nini <160732313+hanv1c@users.noreply.github.com>
2025-01-20 12:08:44 -0300

    Update views.py

--- 11/views.py
+++ 11/views.py
@@ -1,14 +1,18 @@
-from django.http import HttpResponse
-def index (request):
-    return HttpResponse("A view index funcionou, Wow!")
+from django.http import HttpResponse 
 
-# Create your views here.
 
-def sobre(request):
+def index(request):
+    return HttpResponse("A view index funcionou , Wow!")
+
+def sobre (request):
     return HttpResponse("<h1>Sistema 1.0 desenvolvido por mim<h1>")
 
-def contato(request):
-    return HttpResponse("<h1>Esta é a página de contato.<h1>")
+from django.http import HttpResponse 
 
-def ajuda(request):
-    return HttpResponse("<h1>Esta é a página de ajuda.<h1>")
+def contato(request):
+    return  HttpResponse("<h1>Contato</h1><p>ESta é a página de contato.</p>")
+
+from django.http import HttpResponse 
+
+def ajuda(resquest):
+    return HttpResponse("<h1>Ajuda</h1><p>Esta é a página de ajuda.<p/>")
