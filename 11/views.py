a307f0e7f1858ce6512e2e39bc925f7b3401ad68
nini <160732313+hanv1c@users.noreply.github.com>
2025-01-24 09:46:56 -0300

    Update views.py

--- 11/views.py
+++ 11/views.py
@@ -1,8 +1,7 @@
+# Create your views here.
 from django.http import HttpResponse
 def index (request):
     return HttpResponse("A view index funcionou, Wow!")
-
-# Create your views here.
 
 def sobre(request):
     return HttpResponse("<h1>Sistema 1.0 desenvolvido por mim<h1>")
